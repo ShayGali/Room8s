07df06a75cdb595671e1d13a811aabadc732ef61
ShayG0 <86621168+ShayG0@users.noreply.github.com>
2022-04-25 13:28:27 +0300

    add activities

---
+++ front/code/app/src/main/java/com/example/room8/homePageActivity.java
@@ -0,0 +1,14 @@
+package com.example.room8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
