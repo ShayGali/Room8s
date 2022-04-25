2c82b8c197293eb06acb3c0544ab3ac9378f24ea
ShayG0 <86621168+ShayG0@users.noreply.github.com>
2022-04-25 13:36:04 +0300

    add activities

--- front/code/app/src/main/java/com/example/room8/SettingActivity.java
+++ front/code/app/src/main/java/com/example/room8/SettingActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class settingActivity extends AppCompatActivity {
+public class SettingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
