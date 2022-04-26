ecc91184b6483e883a3a3be774e6562fda052a88
ShayG0 <86621168+ShayG0@users.noreply.github.com>
2022-04-26 10:39:12 +0300

    add function that save JWT token and check if he exist

--- front/code/app/src/main/java/com/example/room8/MainActivity.java
+++ front/code/app/src/main/java/com/example/room8/MainActivity.java
@@ -4,17 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.example.room8.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String JWT_SHARED_PREFERENCE = "jwt shared preference";
+    public static final String JWT_TOKEN = "jwt token";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
+
+    // check if we have a token in the SharedPreferences
+    public boolean checkIfJwtTokenExists() {
+        SharedPreferences sp = getSharedPreferences(JWT_SHARED_PREFERENCE, MODE_PRIVATE);
+        return sp.getString(JWT_TOKEN, null) == null;
+    }
+
+    // get a string token and save it to the SharedPreferences
+    public void saveJwtToken(String token) {
+        SharedPreferences sp = getSharedPreferences(JWT_SHARED_PREFERENCE, MODE_PRIVATE);
+        SharedPreferences.Editor ed = sp.edit();
+        ed.putString(JWT_TOKEN, token);
+        ed.apply();
+    }
+
 }
