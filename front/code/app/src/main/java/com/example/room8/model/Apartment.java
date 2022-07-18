a9a410039e0d533c730c8ea1e559f011956a2d2a
ShayGali <shayali100@gmail.com>
2022-07-18 13:01:21 +0300

    add apartment singleton class

---
+++ front/code/app/src/main/java/com/example/room8/model/Apartment.java
@@ -0,0 +1,66 @@
+package com.example.room8.model;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public final class Apartment {
+
+    private static volatile Apartment instance;
+
+    public static Apartment getInstance() {
+        if (instance == null) {
+            synchronized (Apartment.class) {
+                if (instance == null)
+                    instance = new Apartment();
+            }
+        }
+        return instance;
+    }
+
+    private static final String ID_KEY = "";
+    private static final String NAME_KEY = "";
+    private static final String NUMBER_OF_PEOPLE_KEY = "";
+
+    private int id;
+    private String name;
+    private int number_of_people;
+
+    public static void parseDataFromJson(JSONObject apartmentAsJson) throws JSONException {
+        instance = getInstance(); // for create if not exists
+        if (apartmentAsJson.has(ID_KEY) && !apartmentAsJson.isNull(ID_KEY))
+            instance.setId(apartmentAsJson.getInt(ID_KEY));
+        if (apartmentAsJson.has(NAME_KEY))
+            instance.setName(apartmentAsJson.getString(NAME_KEY));
+        if (apartmentAsJson.has(NUMBER_OF_PEOPLE_KEY) && !apartmentAsJson.isNull(NUMBER_OF_PEOPLE_KEY))
+            instance.setNumber_of_people(apartmentAsJson.getInt(NUMBER_OF_PEOPLE_KEY));
+    }
+
+
+    public Apartment() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber_of_people() {
+        return number_of_people;
+    }
+
+    public void setNumber_of_people(int number_of_people) {
+        this.number_of_people = number_of_people;
+    }
+}
