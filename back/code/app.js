9f202a43f6069919e48b224b47b847a17f9d4889
ShayGali <shayali100@gmail.com>
2022-07-19 22:22:19 +0300

    export the message route

--- back/code/app.js
+++ back/code/app.js
@@ -1,85 +1,11 @@
 require("dotenv").config();
 
-const SocketServer = require("websocket").server;
-const app = require("./rest control");
+const restServer = require("./rest control");
 
-const server = require("http").createServer(app);
+const server = require("http").createServer(restServer);
+module.exports = { server };
 
-const { authenticateToken } = require("./middleware/auth");
-const messagingService = require("./service/messagingService");
-const webSocketServer = new SocketServer({
-  httpServer: server,
-  path: "/messages",
-});
-
-let connections = {};
-
-webSocketServer.on("request", async (req) => {
-  const token = authenticateToken(req.httpRequest.headers["x-auth-token"]);
-
-  if (!token) {
-    console.log("reject connection - token invalid");
-    req.reject(401, "invalid token");
-    return;
-  }
-  if (!token.apartmentId) {
-    console.log("reject connection - user dont have apartment");
-    req.reject(403, "user dont have apartment");
-    return;
-  }
-
-  const connection = req.accept();
-  console.log(`New connection`);
-
-  if (connections[token.apartmentId] != undefined) {
-    connections[token.apartmentId].push(connection);
-  } else {
-    connections[token.apartmentId] = [connection];
-  }
-  (await messagingService.getMessages(token.apartmentId, token.userId)).forEach(
-    (msg) => {
-      connection.sendUTF(JSON.stringify(msg));
-    }
-  );
-
-  connection.on("message", (msg) => {
-    connections[token.apartmentId].forEach((element) => {
-      messageWithTime = JSON.parse(msg.utf8Data);
-      let timestamp = new Date().toISOString().slice(0, 19).replace("T", " ");
-      messageWithTime.timestamp = timestamp;
-
-      if (element !== connection) {
-        messageWithTime.isSent = false;
-        element.sendUTF(JSON.stringify(messageWithTime));
-      } else {
-        messagingService
-          .saveMessageToDB(
-            token.apartmentId,
-            token.userId,
-            messageWithTime.message,
-            timestamp,
-            messageWithTime.UUID
-          )
-          .then((response) => {
-            element.sendUTF(JSON.stringify(response));
-          });
-      }
-    });
-  });
-
-  connection.on("close", (resCode, desc) => {
-    console.log(`connection closed. resCode:${resCode}, description: ${desc}`);
-
-    currentApartmentConnections = connections[token.apartmentId];
-
-    currentApartmentConnections = currentApartmentConnections.filter(
-      (element) => element !== connection
-    );
-    if (currentApartmentConnections.length == 0) {
-      delete connections[token.apartmentId];
-    }
-  });
-});
+require("./routes/messageSocket"); // using the message route
 
 const PORT = process.env.PORT || 3000;
 
---
+++ back/code/routes/messageSocket.js
@@ -0,0 +1,80 @@
+const SocketServer = require("websocket").server;
+
+const { server } = require("../app");
+
+const { authenticateToken } = require("../middleware/auth");
+const messagingService = require("../service/messagingService");
+
+const webSocketServer = new SocketServer({
+  httpServer: server,
+  path: "/messages",
+});
+
+const connections = {};
+
+webSocketServer.on("request", async (req) => {
+  const token = authenticateToken(req.httpRequest.headers["x-auth-token"]);
+
+  if (!token) {
+    console.log("reject connection - token invalid");
+    req.reject(401, "invalid token");
+    return;
+  }
+  if (!token.apartmentId) {
+    console.log("reject connection - user dont have apartment");
+    req.reject(403, "user dont have apartment");
+    return;
+  }
+
+  const connection = req.accept();
+  console.log(`New connection`);
+
+  if (connections[token.apartmentId] != undefined) {
+    connections[token.apartmentId].push(connection);
+  } else {
+    connections[token.apartmentId] = [connection];
+  }
+  (await messagingService.getMessages(token.apartmentId, token.userId)).forEach(
+    (msg) => {
+      connection.sendUTF(JSON.stringify(msg));
+    }
+  );
+
+  connection.on("message", (msg) => {
+    connections[token.apartmentId].forEach((element) => {
+      messageWithTime = JSON.parse(msg.utf8Data);
+      let timestamp = new Date().toISOString().slice(0, 19).replace("T", " ");
+      messageWithTime.timestamp = timestamp;
+
+      if (element !== connection) {
+        messageWithTime.isSent = false;
+        element.sendUTF(JSON.stringify(messageWithTime));
+      } else {
+        messagingService
+          .saveMessageToDB(
+            token.apartmentId,
+            token.userId,
+            messageWithTime.message,
+            timestamp,
+            messageWithTime.UUID
+          )
+          .then((response) => {
+            element.sendUTF(JSON.stringify(response));
+          });
+      }
+    });
+  });
+
+  connection.on("close", (resCode, desc) => {
+    console.log(`connection closed. resCode:${resCode}, description: ${desc}`);
+
+    currentApartmentConnections = connections[token.apartmentId];
+
+    currentApartmentConnections = currentApartmentConnections.filter(
+      (element) => element !== connection
+    );
+    if (currentApartmentConnections.length == 0) {
+      delete connections[token.apartmentId];
+    }
+  });
+});
