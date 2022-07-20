c0ece8b09e5b26706410ed76f65c449492e3c530
ShayGali <shayali100@gmail.com>
2022-07-20 08:51:22 +0300

    update

--- back/code/app.js
+++ back/code/app.js
@@ -5,8 +5,8 @@ const restServer = require("./rest control");
 const server = require("http").createServer(restServer);
 module.exports = { server };
 
-require("./routes/messageSocket"); // using the message route
-
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Listening in port ${PORT}`));
+
+require("./routes/messageSocket"); // using the message route
