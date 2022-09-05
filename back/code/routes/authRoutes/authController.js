199a36302c3d4211a646550164b47810d9d6f8ad
Shay Gali <86621168+ShayGali@users.noreply.github.com>
2022-09-05 18:13:30 +0300

    start of forget password

--- back/code/routes/authRoutes/authController.js
+++ back/code/routes/authRoutes/authController.js
@@ -1,11 +1,18 @@
+import { v4 as uuidv4 } from "uuid";
+
 const authService = require("./authService");
 const userService = require("../../routes/userRoutes/userService");
 
 const bcrypt = require("bcrypt");
 
-const { generateAccessToken,generateRefreshToken } = require("../../utilities/jwtHandler");
-const { isStrongPassword } = require("../../utilities/passwordHandler");
+const {
+  generateAccessToken,
+  generateRefreshToken,
+} = require("../../utilities/jwtHandler");
+const { isStrongPassword, hashPassword } = require("../../utilities/passwordHandler");
 const valuesValidate = require("../../utilities/valuesValidate");
+
+const resetTokenMap = new Map();
 
 // TODO: check if user name is exist
 exports.register = async (req, res, next) => {
@@ -47,7 +54,6 @@ exports.register = async (req, res, next) => {
       userId: result.insertId,
       apartmentId: null,
     });
-    
 
     return res.status(201).json({ success: true, msg: "success", jwtToken });
   } catch (err) {
@@ -81,7 +87,7 @@ exports.login = async (req, res, next) => {
     const refreshJwtToken = generateRefreshToken({
       userId: findUser.ID,
       apartmentId: apartmentId !== undefined ? apartmentId : null,
-    })
+    });
 
     return res.status(200).json({
       success: true,
@@ -98,4 +104,61 @@ exports.login = async (req, res, next) => {
 
 exports.hello = async (req, res, next) => {
   return res.send({ success: true, msg: "hello", jwtToken: req.tokenData });
+};
+
+exports.forgotPassword = async (req, res, next) => {
+  const { email } = req.body;
+
+  if (email === undefined) {
+    return res.status(400).json({ success: false, msg: "send email" });
+  }
+
+  if ((await userService.findByEmail(email)) === undefined)
+    return res
+      .status(404)
+      .json({ success: false, msg: `user with the email ${email} not found` });
+
+  const resetTokenUUID = uuidv4();
+  const expirityDate = generateExprityDate();
+
+  resetTokenMap.set(resetTokenUUID, { expirityDate, email });
+
+  sendEmail(email, resetTokenUUID);
+
+  return res.send({ success: true, msg: "reset token send to ypur email" });
+};
+
+// genrate a expirtion time
+function generateExprityTime() {
+  const EXPIRATION_TIME_IN_MINUTES = 15;
+  return new Date().getTime() + EXPIRATION_TIME_IN_MINUTES * 60000;
+}
+
+// check if the token of the email expired
+function checkIfExpired(time) {
+  return time - Date.now() < 0;
+}
+
+// TODO
+function sendEmail(email, token) {}
+
+exports.resetPassword = async (req, res, next) => {
+  const { token, password } = req.body;
+
+  if (!token)
+    return res.status(401).json({ success: false, msg: "send token" });
+
+  if (password===undefined || !isStrongPassword(password)) 
+    return res.status(400).json({ success: false, msg: "password not valid" });
+  
+  const {expirtionTime, email} = resetTokenMap.get(token)
+
+  if(checkIfExpired(expirtionTime))
+    return res.status(410).json({success:false, msg:"token expired"})
+
+
+  const hashedPassword = await hashPassword(password)
+  userService.updatePassword(email, hashedPassword); // TODO
+
+  return res.json({success:true, msg:"password changed"})
 };
--- back/code/routes/authRoutes/index.js
+++ back/code/routes/authRoutes/index.js
@@ -7,6 +7,10 @@ const { authenticateTokenFromRequest } = require("../../middleware/auth");
 
 router.route("/login").post(authController.login);
 router.route("/register").post(authController.register);
+router.route("/forgotPassword")
+.get()
+.post(authController.forgotPassword)
+;
 router.route("/hello").get(authenticateTokenFromRequest, authController.hello);
 
 module.exports = router;
