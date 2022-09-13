0c9e62325d0b5057e32396e0c20c6623c7463e98
Shay Gali <86621168+ShayGali@users.noreply.github.com>
2022-09-13 17:46:31 +0300

    start profile img

--- front/code/app/src/main/java/com/example/room8/ImageFactory.java
+++ front/code/app/src/main/java/com/example/room8/ImageFactory.java
@@ -4,15 +4,30 @@ Factory fot images resources
  */
 public final class ImageFactory{
 
+    public static final int[] manImgs = new int[]{};
+    public static final int[] womanImgs = new int[]{};
+    public static final int[] ungenderAndAnimalsImgs = new int[]{};
 
-//    public static int profileImageFactory(int iconId){
-//        switch (iconId){
-//            case 1:
-//                return R.drawable.task_64;
-//            default:
-//                return R.drawable.task_64;
-//        }
-//    }
+   public static int profileImageFactory(int iconId){
+       if(iconId < 0)
+        return defaultImg;
+    
+        if(iconId < manImgs.length)
+            return manImgs[iconId];
+
+        iconId = iconId - manImgs.length;
+
+        if(iconId < womanImgs.length)
+            return womanImgs[iconId];
+
+        iconId = iconId - womanImgs.length;
+
+        if(iconId < ungenderAndAnimalsImgs.length)
+            return ungenderAndAnimalsImgs[iconId];
+
+        return defaultImg;
+
+   }
 //
 //    public static int taskImageFactory(int iconId){
 //        switch (iconId){
