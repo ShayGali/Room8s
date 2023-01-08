4d5b121e0d896a948c12932dec802b1e527c60cc
avial2013 <avicop@gmail.com>
2023-01-08 22:04:24 +0200

    Nodemailer Fix

--- back/code/config/nodeMailer.js
+++ back/code/config/nodeMailer.js
@@ -1,6 +1,9 @@
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     service: "Zoho",
+    host:'smtp.zoho.com',
+    secure:true,
+    port:465,
     auth: {
       user: process.env.ADMIN_MAIL,
       pass: process.env.ADMIN_PASS,
